4931b56baa8a68df731cbe2c760559c8129aecb3
Ignacio Nin <ignacio@remind101.com>
2018-05-24 20:20:57 -0300

    Allow overriding baseclass for yaml tests

--- stacker_blueprints/test/__init__.py
+++ stacker_blueprints/test/__init__.py
@@ -14,8 +14,35 @@ class YamlDirTestGenerator():
         if not os.path.isdir(self.classdir):
             self.classdir = os.path.dirname(self.classdir)
 
+    @property
+    def base_class(self):
+        return BlueprintTestCase
+
     def test_generator(self):
         configs = glob('%s/test_*.yaml' % self.classdir)
+
+        class ConfigTest(self.base_class):
+            def __init__(self, config, stack, filepath):
+                self.config = config
+                self.stack = stack
+                self.description = "%s (%s)" % (stack.name, filepath)
+
+            def __call__(self):
+                ctx = Context(config=self.config,
+                              environment={'environment': 'test'})
+
+                configvars = self.stack.variables or {}
+                variables = [Variable(k, v) for k, v in configvars.iteritems()]
+
+                blueprint_class = load_object_from_string(
+                    self.stack.class_path)
+                blueprint = blueprint_class(self.stack.name, ctx)
+                blueprint.resolve_variables(variables or [])
+                blueprint.create_template()
+                self.assertRenderedBlueprint(blueprint)
+
+            def assertEquals(self, a, b, msg):
+                assert a == b, msg
 
         for f in configs:
             with open(f) as test:
@@ -32,25 +59,3 @@ class YamlDirTestGenerator():
                     #
                     # http://nose.readthedocs.io/en/latest/writing_tests.html#test-generators
                     yield ConfigTest(config, stack, filepath=f)
-
-
-class ConfigTest(BlueprintTestCase):
-    def __init__(self, config, stack, filepath):
-        self.config = config
-        self.stack = stack
-        self.description = "%s (%s)" % (stack.name, filepath)
-
-    def __call__(self):
-        ctx = Context(config=self.config, environment={'environment': 'test'})
-
-        configvars = self.stack.variables or {}
-        variables = [Variable(k, v) for k, v in configvars.iteritems()]
-
-        blueprint_class = load_object_from_string(self.stack.class_path)
-        blueprint = blueprint_class(self.stack.name, ctx)
-        blueprint.resolve_variables(variables or [])
-        blueprint.create_template()
-        self.assertRenderedBlueprint(blueprint)
-
-    def assertEquals(self, a, b, msg):
-        assert a == b, msg
